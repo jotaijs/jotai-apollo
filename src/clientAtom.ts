44620075849e3c76e873a5dab0cdaf83228cd40c
aslemammad <zorofight94@gmail.com>
2021-12-01 23:05:39 +0330

    clientAtom

---
+++ src/clientAtom.ts
@@ -0,0 +1,13 @@
+import { InMemoryCache, ApolloClient } from '@apollo/client'
+import { atom } from 'jotai'
+
+const DEFAULT_URL =
+  (typeof process === 'object' && process.env.JOTAI_URQL_DEFAULT_URL) ||
+  '/graphql'
+
+const client = new ApolloClient({
+  uri: DEFAULT_URL,
+  cache: new InMemoryCache(),
+})
+
+export const clientAtom = atom(client)
