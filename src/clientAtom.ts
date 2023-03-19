aad94cc1fe0141e88e27e298953f2068c3060df8
Iwo Plaza <gobbobminecraft@gmail.com>
2023-03-19 19:33:09 +0100

    fix: the default Apollo Client is now constructed on demand to avoid confu… (#5)
    * The default Apollo Client is now constructed on demand to avoid confusing the Apollo DevTools.
    
    * Reimplemented an ability to override the default client by setting the atom.
    
    * Fixed null check

--- src/clientAtom.ts
+++ src/clientAtom.ts
@@ -1,13 +1,36 @@
-import { InMemoryCache, ApolloClient } from '@apollo/client'
+import {
+  InMemoryCache,
+  ApolloClient,
+  NormalizedCacheObject,
+} from '@apollo/client'
 import { atom } from 'jotai/vanilla'
 
 const DEFAULT_URL =
   (typeof process === 'object' && process.env.JOTAI_APOLLO_DEFAULT_URL) ||
   '/graphql'
 
-const client = new ApolloClient({
-  uri: DEFAULT_URL,
-  cache: new InMemoryCache(),
-})
+let defaultClient: ApolloClient<NormalizedCacheObject> | null = null
 
-export const clientAtom = atom(client)
+const customClientAtom = atom<ApolloClient<unknown> | null>(null)
+
+export const clientAtom = atom(
+  (get) => {
+    const customClient = get(customClientAtom)
+
+    if (customClient) {
+      return customClient
+    }
+
+    if (!defaultClient) {
+      defaultClient = new ApolloClient({
+        uri: DEFAULT_URL,
+        cache: new InMemoryCache(),
+      })
+    }
+
+    return defaultClient
+  },
+  (_get, set, client: ApolloClient<unknown>) => {
+    set(customClientAtom, client)
+  }
+)
